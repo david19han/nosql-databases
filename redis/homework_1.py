7de838764d02de2aaef63bbf9efa2f0108557093
David Han <dth2126@columbia.edu>
2018-04-02 20:09:53 -0400

    corrected date

--- redis/homework_1.py
+++ redis/homework_1.py
@@ -1,5 +1,5 @@
 import requests
-parameters = {"date": "1995-11-19"}
+parameters = {"date": "2017-11-19"}
 response = requests.get("https://api.nasa.gov/planetary/apod?api_key=3hWwkDdZ1xdwjTNpKO2nmqkTHexZ7r9K8hyKS8Q4", params=parameters)
 data = response.json()
 print(data["url"])
