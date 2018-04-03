3120727bf6b2bbd55b675fc55920f9d23ac6ec7d
David Han <dth2126@columbia.edu>
2018-04-02 20:49:39 -0400

    added comment

--- redis/homework_1.py
+++ redis/homework_1.py
@@ -1,3 +1,4 @@
+#Please note that in order for this script to work "requests" module must be installed. Thus, "pip install requests" will need to be done before running this script.
 import requests
 parameters = {"date": "2017-11-19"}
 response = requests.get("https://api.nasa.gov/planetary/apod?api_key=3hWwkDdZ1xdwjTNpKO2nmqkTHexZ7r9K8hyKS8Q4", params=parameters)
